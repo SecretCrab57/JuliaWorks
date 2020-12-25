1436bab102d3282ff8b33abe6ccb8539f23e2e60
SecretCrab57 <70604193+SecretCrab57@users.noreply.github.com>
2020-12-25 03:00:52 +0300

    Update 10.jl

--- 10.jl
+++ 10.jl
@@ -1,32 +1,40 @@
 count = 0
 
-function great_painter(r::Robot)
-    global count=0
-    overal_temp=0
-    side=Ost
-    while isborder(r,Nord)==false
-        overal_temp+=row_temp(r,side)
-        move!(r,Nord)
-        side=inverse(side)
+function mister_robot(r::Robot)
+
+    global count = 0
+    overal_temp = 0
+    side = Ost
+
+    while isborder(r, Nord) == false
+        overal_temp+=row_temp(r, side)
+        move!(r, Nord)
+        side = inverse(side)
     end
-    overal_temp+=row_temp(r,side)
-    average_temp=overal_temp/count
+
+    overal_temp+=row_temp(r, side)
+    average_temp = overal_temp / count
     return average_temp
 end
 
 function row_temp(r::Robot, side::HorizonSide)
-    row_temp=0
-    while isborder(r,side)==false
+
+    row_temp = 0
+
+    while isborder(r, side) == false
         row_temp+=check_temp(r)
-        move!(r,side)
+        move!(r, side)
     end
+
     row_temp+=check_temp(r)
     return row_temp
 end
 
 function check_temp(r::Robot)
+
     global count
-    if ismarker(r)==true
+
+    if ismarker(r) == true
         count+=1
         return temperature(r)
     else
@@ -34,4 +42,4 @@ function check_temp(r::Robot)
     end
 end
 
-inverse(side::HorizonSide)=HorizonSide(mod(Int(side)+2,4))
+inverse(side::HorizonSide) = HorizonSide(mod(Int(side) + 2, 4))
